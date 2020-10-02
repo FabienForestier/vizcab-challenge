a76bc646a6371d4cda5920b5d8dfca41f5f966d3
unknown <fab.forestier42@gmail.com>
2020-10-02 18:35:51 +0200

    Added sliders inputs in order to be able to modify housing number and living ratio

--- src/components/CardOccupationZone.vue
+++ src/components/CardOccupationZone.vue
@@ -2,8 +2,54 @@
   <v-card color="primary" dark>
     <v-card-title>{{ zone.Nom }}</v-card-title>
     <v-card-text>
-      <span>Nombre de logements : {{ zone.Nb_logement }}</span><br />
-      <span>Ratio habitable : {{ zone.Ratio_habitable }} </span><br />
+      <v-row>
+        <v-col class="pr-4">
+          <span>Nombre de logements :</span><br />
+          <v-slider
+            v-model="housingNumber"
+            class="align-center"
+            max="50"
+            min="1"
+            hide-details
+          >
+            <template v-slot:append>
+              <v-text-field
+                v-model="housingNumber"
+                class="mt-0 pt-0"
+                hide-details
+                single-line
+                type="number"
+                style="width: 60px"
+              ></v-text-field>
+            </template>
+          </v-slider>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col class="pr-4">
+          <span>Ratio habitable :</span><br />
+          <v-slider
+            v-model="ratio"
+            class="align-center"
+            max="1"
+            min="0"
+            step="0.1"
+            hide-details
+          >
+            <template v-slot:append>
+              <v-text-field
+                v-model="ratio"
+                class="mt-0 pt-0"
+                hide-details
+                single-line
+                type="number"
+                style="width: 60px"
+              ></v-text-field>
+            </template>
+          </v-slider>
+        </v-col>
+      </v-row>
       <span>Surface utile (m²) : {{ zoneUsableArea }}</span
       ><br />
       <span>Surface habitable (m²) : {{ zoneLivingArea }}</span
@@ -21,6 +67,13 @@
 <script>
 export default {
   name: "CardOccupationZone",
+  data: function() {
+    return {
+      // Those variables can be modified by the sliders
+      housingNumber: 1,
+      ratio: 0.7,
+    };
+  },
   props: {
     zone: Object,
   },
@@ -32,12 +85,12 @@ export default {
     zoneLivingArea: function() {
       // The living area of a zone is computed like this
       // LivingArea = UsableArea * LivingRatio
-      return this.zoneUsableArea * this.zone.Ratio_habitable;
+      return this.zoneUsableArea * this.ratio;
     },
     averageAreaPerHousing: function() {
       // The average area per housing is computed this way
       // AverageArePerHousing = LivingArea / zoneNumberOfHousing
-      return this.zoneLivingArea / this.zone.Nb_logement;
+      return this.zoneLivingArea / this.housingNumber;
     },
     maxAdultsPerHousing: function() {
       let maxAdultNb = 0;
@@ -56,13 +109,19 @@ export default {
       let inhabitants;
 
       if (this.maxAdultsPerHousing < 1.75) {
-        inhabitants = this.zone.Nb_logement *this.maxAdultsPerHousing;
+        inhabitants = this.housingNumber *this.maxAdultsPerHousing;
       } else {
-        inhabitants = this.zone.Nb_logement * (1.75 + 0.3 * (this.maxAdultsPerHousing - 1.75));
+        inhabitants = this.housingNumber * (1.75 + 0.3 * (this.maxAdultsPerHousing - 1.75));
       }
 
       return inhabitants;
     }
+  },
+  created() {
+    // Initializing housingNumber and ration with the known value
+    // for the zone
+    this.housingNumber = this.zone.Nb_logement;
+    this.ratio = this.zone.Ratio_habitable;
   }
 };
 </script>
